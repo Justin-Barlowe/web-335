3b907c20abd9ff962a76f7a16f4788f29bf8f41b
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-09-07 13:23:05 -0400

    Week 5 Assignment

---
+++ week_5/barlowe_assignment5.js
@@ -0,0 +1,16 @@
+// Name: Justin Barlowe
+// Title: barlowe-assignment5.3.js
+// Description: Mongo DB queries for week 4 assignment 5.2
+// Date: 9/7/2023
+
+//Create new user
+db.users.insertOne({ "firstName": "Joseph", "lastName": "Haydn", "employeeId": "1013", "email": "jhadyn@me.com"
+, "dateCreated": new Date() })
+
+//Update Mozart's email
+db.users.updateOne({ "lastName: "Mozart" }, { $set: { "email": "mozart@me.com" } } )
+)
+
+// Find users using projection
+db.users.find({}, { firstName: 1, lastName: 1, email: 1, _id: 0 })
+
